d369cc67a98cb3c37134523a4a5490ce2d1177a0
ANANYAVINOD <ananya@qburst.com>
2020-10-02 12:15:23 +0530

    Responsive changes, contact update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,4 @@
+
 let menuData = new XMLHttpRequest();
 menuData.open("GET", "http://127.0.0.1:8000/menu.json");
 menuData.send();
@@ -9,7 +10,7 @@ function loadMenu(){
     for(let i=0;i<menu.length;i++){
         const listItem = document.createElement('li');
         const listItemAnchor = document.createElement('a');
-        if(menu['not_found'] === true){
+        if(menu['notFound'] === true){
             listItemAnchor.href = 'pageNotFound.html';
         }
         else{
@@ -21,4 +22,18 @@ function loadMenu(){
     }
     const nav = document.getElementById('nav');
     nav.appendChild(list);
+    updateTitle(menu);
 }
+
+function updateTitle(menuData) {
+    const filePath = window.location.pathname.slice(1);
+    menuData.forEach(element => {
+        if (element['path'] === filePath) {
+            const pageHeading = document.getElementById('page-heading');
+            pageHeading.innerHTML = element['label'];
+            const pageTitle = document.querySelector('title');
+            pageTitle.innerHTML = element['label'];
+        }
+    });
+}
+
